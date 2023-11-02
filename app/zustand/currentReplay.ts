ba646a1910b39c62cae5ba1f3af5663f51563a6f
Horikaze <adrianyt659@gmail.com>
2023-11-02 19:48:05 +0100

    small fixed

--- app/zustand/currentReplay.ts
+++ app/zustand/currentReplay.ts
@@ -4,12 +4,12 @@ import { create } from "zustand";
 interface CurrentReplay {
   replay: Replay | null;
   changeReplay: (replay: Replay) => void;
-  clear: (replay: Replay) => void;
+  clear: () => void;
 }
 
 const currentReplay = create<CurrentReplay>((set) => ({
   replay: null,
-  changeReplay: (replay) => set((state) => ({ replay: replay })),
-  clear: (replay) => set((state) => ({ replay: null })),
+  changeReplay: (replay) => set(() => ({ replay: replay })),
+  clear: () => set({ replay: null }),
 }));
 export default currentReplay;
