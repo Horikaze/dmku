4273583794613d5edea2ff601016af602b620943
Horikaze <adrianyt659@gmail.com>
2023-11-04 13:28:15 +0100

    added mobile footer

--- app/components/Navigation/Sidebar.tsx
+++ app/components/Navigation/Sidebar.tsx
@@ -1,10 +1,12 @@
-import DesktopSidebar from "./components/navigation/desktopSidebar/DesktopSidebar";
+import DesktopSidebar from "./desktopSidebar/DesktopSidebar";
+import MobileFooter from "./mobileFooter/MobileFooter";
 
 const Sidebar = async ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex flex-row h-full">
+    <div className="flex flex-col lg:flex-row h-full">
       <DesktopSidebar />
       <main className="h-full">{children}</main>
+      <MobileFooter />
     </div>
   );
 };
---
+++ app/components/Navigation/mobileFooter/MobileFooter.tsx
@@ -0,0 +1,7 @@
+const MobileFooter = () => {
+  return (
+    <div className="lg:hidden flex flex-col h-20 w-full bg-white">MobileFooter</div>
+  );
+};
+
+export default MobileFooter;
