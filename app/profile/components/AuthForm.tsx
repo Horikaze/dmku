3ea0d3a9dd36014278903e0e287cc1403ef316a5
Horikaze <adrianyt659@gmail.com>
2023-11-05 17:35:14 +0100

    updated form

--- app/profile/components/AuthForm.tsx
+++ app/profile/components/AuthForm.tsx
@@ -1,62 +1,145 @@
 "use client";
+import Input from "@/app/profile/components/Input";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useCallback, useMemo, useState } from "react";
 import { useForm } from "react-hook-form";
+import { FaDiscord, FaGithub, FaAngleRight } from "react-icons/fa6";
 import * as z from "zod";
-
-const formSchema = z
-  .object({
-    name: z
-      .string()
-      .min(3, { message: "The name must be at least 3 characters." })
-      .max(15, { message: "The name must be less than 15 characters." })
-      .regex(/^[a-zA-Z0-9_]+$/),
-    password: z
-      .string()
-      .min(3, { message: "The password must be at least 3 characters." })
-      .max(15, { message: "The name must be less than 15 characters." })
-      .regex(/^[a-zA-Z0-9_]+$/),
-    confirmPassword: z
-      .string()
-      .min(3, { message: "The password must be at least 3 characters." })
-      .max(15, { message: "The name must be less than 15 characters." })
-      .regex(/^[a-zA-Z0-9_]+$/),
-  })
-  .refine((data) => data.password === data.confirmPassword, {
-    message: "Passwords are not the same.",
-    path: ["confirmPassword"],
-  });
+import AuthSocialButton from "./AuthSocialButton";
+type Variant = "LOGIN" | "REGISER";
 
 const AuthForm = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [variant, setVariant] = useState<Variant>("LOGIN");
+
+  const formSchema = useMemo(() => {
+    return z
+      .object({
+        password: z
+          .string()
+          .min(3, { message: "The password must be at least 3 characters." })
+          .max(15, { message: "The name must be less than 15 characters." }),
+        confirmPassword: z
+          .string()
+          .max(15, { message: "The name must be less than 15 characters." }),
+        nickname: z
+          .string()
+          .min(3, { message: "The name must be at least 3 characters." })
+          .max(15, { message: "The name must be less than 15 characters." }),
+      })
+      .superRefine((i, ctx) => {
+        if (variant === "REGISER" && i.password !== i.confirmPassword) {
+          ctx.addIssue({
+            code: z.ZodIssueCode.custom,
+            message: `Passwords are not the same.`,
+            path:["confirmPassword"]
+          });
+        }
+      });
+  }, [variant]);
   const {
     register,
     handleSubmit,
     reset,
+    setValue,
     formState: { errors },
   } = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      name: "",
       password: "",
       confirmPassword: "",
+      nickname: "",
     },
   });
 
+  const toggleVariants = useCallback(() => {
+    if (variant === "LOGIN") {
+      setVariant("REGISER");
+    } else if (variant === "REGISER") {
+      setVariant("LOGIN");
+      setValue("confirmPassword", "");
+    }
+  }, [setValue, variant]);
   const pocessForm = async (data: z.infer<typeof formSchema>) => {
     console.log(data);
   };
 
+  const socialAction = (action: string) => {
+    console.log(action);
+  };
+
   return (
-    <div>
-      <form
-        onSubmit={handleSubmit(pocessForm)}
-        className="flex flex-col gap-y-2 mt-6"
-      >
-        <input type="text" {...register("name")} />
-        <input type="password" {...register("password")} />
-        <input type="password" {...register("confirmPassword")} />
-        <button type="submit">OK</button>
-      </form>
-    </div>
+    <form
+      onSubmit={handleSubmit(pocessForm)}
+      className="flex w-full max-w-md flex-col gap-y-2 mt-6 rounded-md bg-[#2a303c] px-7 py-3 "
+    >
+      <Input
+        label="Nickname"
+        register={register}
+        id="nickname"
+        errors={errors}
+        disabled={isLoading}
+      />
+
+      <Input
+        errors={errors}
+        register={register}
+        id="password"
+        type="password"
+        disabled={isLoading}
+        label="Password"
+      />
+      {variant === "REGISER" ? (
+        <Input
+          errors={errors}
+          register={register}
+          id="confirmPassword"
+          type="password"
+          disabled={isLoading}
+          required={false}
+          label="Confirm Password"
+        />
+      ) : null}
+      <div className="flex row-auto justify-end">
+        <button
+          type="submit"
+          className="bg-slate-100 hover:bg-slate-300 transition flex flex-row text-sm items-center py-2 px-4 rounded-lg mt-2"
+        >
+          <p>continue</p>
+          <FaAngleRight className="w-4 h-4" />
+        </button>
+      </div>
+      <div className="mt-2 ">
+        <div className="relative">
+          <div className="absolute inset-0 flex items-center">
+            <div className="w-full border-t border bg-gray-300" />
+          </div>
+          <div className="relative flex justify-center text-sm">
+            <span className="bg-[#2a303c] px-2 text-slate-100">
+              Or continue with
+            </span>
+          </div>
+        </div>
+        <div className="mt-6 flex gap-2">
+          <AuthSocialButton
+            icon={FaDiscord}
+            onClick={() => socialAction("discord")}
+          />
+          <AuthSocialButton
+            icon={FaGithub}
+            onClick={() => socialAction("github")}
+          />
+        </div>
+        <div className="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-400">
+          <div>
+            {variant === "LOGIN" ? "New to Dmku?" : "Already have a account?"}
+          </div>
+          <div onClick={toggleVariants} className="underline cursor-pointer">
+            {variant === "LOGIN" ? "Create a account" : "Login"}
+          </div>
+        </div>
+      </div>
+    </form>
   );
 };
 
---
+++ app/profile/components/Input.tsx
@@ -0,0 +1,61 @@
+"use client";
+import clsx from "clsx";
+import { FieldValues, FieldErrors, UseFormRegister } from "react-hook-form";
+
+type formSchemaProps = {
+  nickname: string;
+  password: string;
+  confirmPassword: string;
+};
+interface InputProps {
+  label: string;
+  id: "password" | "confirmPassword" | "nickname";
+  type?: string;
+  required?: boolean;
+  register: UseFormRegister<formSchemaProps>;
+  errors: FieldErrors;
+  disabled?: boolean;
+  placeholder?: string;
+}
+
+const Input = ({
+  label,
+  id,
+  type,
+  required,
+  register,
+  errors,
+  disabled,
+  placeholder,
+}: InputProps) => {
+  return (
+    <div>
+      <label
+        className="block text-sm font-medium leading-6 text-white"
+        htmlFor={id}
+      >
+        {label}
+      </label>
+      <div className="mt-2">
+        <input
+          type={type}
+          placeholder={placeholder}
+          id={id}
+          autoComplete={id}
+          disabled={disabled}
+          {...register(id)}
+          className={clsx(
+            `
+          form-input block w-full rounded-sm border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300
+          placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6
+          `,
+            errors[id] ? "focus:ring-rose-500" : "focus:ring-sky-600",
+            disabled && "opacity-50 cursor-default"
+          )}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Input;
