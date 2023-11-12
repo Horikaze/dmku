8785439a01ca66c9bcc26bee93320c63d7266c09
Horikaze <adrianyt659@gmail.com>
2023-11-12 18:13:05 +0100

    revert

---
+++ lib/getRankingData.ts
@@ -0,0 +1,4 @@
+export const getRankingData = (data: string) => {
+    
+
+};
--- next.config.js
+++ next.config.js
@@ -8,6 +8,9 @@ const nextConfig = {
       {
         hostname: "utfs.io",
       },
+      {
+        hostname: "static.zerochan.net",
+      },
     ],
   },
 };
