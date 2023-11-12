75e5bebde5183de7dc722a90dd6d48a55ecaad1e
Horikaze <adrianyt659@gmail.com>
2023-11-12 23:19:43 +0100

    fixed prisma

--- lib/getRankingData.ts
+++ lib/getRankingData.ts
@@ -1,4 +1,23 @@
-export const getRankingData = (data: string) => {
-    
+export type ScoreObject = {
+  difficulty: string;
+  score: number;
+  id: number;
+}
 
+export const parseScoreString = (scoreString: string): ScoreObject[] => {
+  const scoreParts = scoreString.split("+");
+
+  const scoreObjects = scoreParts.map((part) => {
+    const [difficulty, scoreStr, idStr] = part
+      .split("/")
+      .map((item) => (isNaN(item as any) ? item : item.trim()));
+
+    // Use parseFloat to convert the string to a number
+    const score = parseFloat(scoreStr);
+    const id = parseFloat(idStr);
+
+    return { difficulty, score, id };
+  });
+
+  return scoreObjects;
 };
