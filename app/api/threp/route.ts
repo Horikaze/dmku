531f73d5b0bd2bc55f9a3dc69b2e9be2468e7918
Horikaze <adrianyt659@gmail.com>
2023-11-13 23:10:32 +0100

    updated threp route

--- app/api/threp/route.ts
+++ app/api/threp/route.ts
@@ -1,15 +1,28 @@
-import { ScoreObject } from "@/lib/getRankingData";
+import { ReplayInfo } from "@/app/types/Replay";
 import axios from "axios";
+import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
+import { authOptions } from "../auth/[...nextauth]/route";
 export async function POST(request: NextRequest, response: NextResponse) {
-  const body = await request.formData();
-  const replayFile = body.get("replay") as File;
-  const formData = new FormData();
-  formData.append("replay", replayFile);
-  const res = await axios.post(
-    process.env.NEXT_PUBLIC_APITHREP as string,
-    formData
-  );
-  const replayData = (await res.data()) as ScoreObject;
-  return NextResponse.json(replayData);
+  try {
+    const session = getServerSession(authOptions);
+
+    if (!session) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const body = await request.formData();
+    const replayFile = body.get("replay") as File;
+    const formData = new FormData();
+    formData.append("replay", replayFile);
+    const res = await axios.post(
+      process.env.NEXT_PUBLIC_APITHREP as string,
+      formData
+    );
+    const replayData = (await res.data()) as ReplayInfo;
+    return NextResponse.json(replayData);
+  } catch (error) {
+    console.log(error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
 }
---
+++ app/types/Replay.ts
@@ -0,0 +1,17 @@
+export type ReplayInfo = {
+    c_keys: number;
+    character: string | string[];
+    date: string;
+    frame_count: number;
+    player: string;
+    rank: string;
+    rpy_name: string;
+    shift: number;
+    shottype: string;
+    slow_rate: number;
+    stage: string;
+    stage_score: number[];
+    x_keys: number;
+    z_keys: number;
+  };
+  
