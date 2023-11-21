a15fe6867091e5d59a7d6e57cbaaa97cbc048a4c
Horikaze <adrianyt659@gmail.com>
2023-11-21 22:32:35 +0100

    changed theme

--- app/profile/components/ProfileNavigation.tsx
+++ app/profile/components/ProfileNavigation.tsx
@@ -50,7 +50,7 @@ export default function ProfileNavigation() {
               <Link
                 href={`/profile`}
                 key={label}
-                className={`py-3 px-8 flex flex-row gap-x-1 items-center md:px-10 cursor-pointer hover:bg-secondary rounded ${
+                className={`py-3 px-4 flex flex-row gap-x-1 items-center md:px-10 cursor-pointer hover:bg-secondary rounded ${
                   active && "bg-card"
                 }`}
               >
@@ -63,7 +63,7 @@ export default function ProfileNavigation() {
             <Link
               href={`/profile/${label.toLocaleLowerCase()}`}
               key={label}
-              className={`py-3 px-8 flex flex-row gap-x-1 items-center md:px-10 cursor-pointer hover:bg-secondary rounded ${
+              className={`py-3 px-4 flex flex-row gap-x-1 items-center md:px-10 cursor-pointer hover:bg-secondary rounded ${
                 active && "bg-card"
               }`}
             >
