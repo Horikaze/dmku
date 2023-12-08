20f9143234c9b2cf3a59885300c76b1169b4115e
Horikaze <adrianyt659@gmail.com>
2023-12-08 21:19:00 +0100

    [FE] added missing ModeToggle at mobileNav

--- app/components/Navigation/MobileNav.tsx
+++ app/components/Navigation/MobileNav.tsx
@@ -1,16 +1,29 @@
 import { useRoutes } from "@/app/hooks/useRoutes";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { signOut, useSession } from "next-auth/react";
+import { useTheme } from "next-themes";
 import Link from "next/link";
 import { useState } from "react";
 import { FaSignInAlt, FaUser } from "react-icons/fa";
 import { IoMenuOutline } from "react-icons/io5";
+import { ModeToggle } from "../ModeToggle";
 
 export default function MobileNav() {
   const session = useSession();
   const isAuth = session.status === "authenticated";
   const routes = useRoutes();
   const [open, setOpen] = useState(false);
+
+  const { setTheme, theme } = useTheme();
+
+  const handleClick = () => {
+    if (theme === "dark") {
+      setTheme("light");
+    }
+    if (theme === "light") {
+      setTheme("dark");
+    }
+  };
   return (
     <div className="group md:hidden relative flex h-full flex-col py-2 justify-center items-center hover:brightness-110 transition">
       <div className="flex flex-row gap-x-3 items-center">
@@ -26,8 +39,8 @@ export default function MobileNav() {
                 }}
                 href={"/profile"}
                 className={`rounded-md flex gap-x-2 items-center bg-secondary cursor-pointer ${
-                  routes.at(-1)?.active ? "brightness-150" : ""
-                } hover:brightness-150 p-3 transition-all`}
+                  routes.at(-1)?.active ? "brightness-75" : ""
+                } hover:brightness-75 p-3 transition-all`}
               >
                 <FaUser />
                 <p>Profile</p>
@@ -41,8 +54,8 @@ export default function MobileNav() {
                       setOpen(false);
                     }}
                     className={`rounded-md flex gap-x-2 items-center bg-secondary cursor-pointer ${
-                      active ? "brightness-150" : ""
-                    } hover:brightness-150 p-3 transition-all`}
+                      active ? "brightness-75" : ""
+                    } hover:brightness-75 p-3 transition-all`}
                     key={label}
                   >
                     <Icon />
@@ -51,13 +64,19 @@ export default function MobileNav() {
                 );
               })}
               <div
+                onClick={handleClick}
+                className={`rounded-md gap-x-2 items-center bg-secondary cursor-pointer  hover:brightness-75 p-3 transition-all`}
+              >
+                <ModeToggle />
+              </div>
+              <div
                 onClick={() => {
                   setOpen(false);
                   signOut();
                 }}
                 className={`rounded-md ${
                   isAuth ? "flex" : "hidden"
-                } gap-x-2 items-center bg-secondary cursor-pointer  hover:brightness-150 p-3 transition-all`}
+                } gap-x-2 items-center bg-secondary cursor-pointer hover:brightness-75 p-3 transition-all`}
               >
                 <FaSignInAlt />
                 <p>Logout</p>
